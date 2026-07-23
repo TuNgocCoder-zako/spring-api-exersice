5a39678d111f732d2cdb2c0564d4cd8af85defb2
TuNgoc882005 <tu.nn.65cntt@ntu.edu.vn>
2026-07-23 09:48:20 +0700

    code 23-7

---
+++ src/main/java/com/example/demo/entity/Role.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+     UUID id;
+     String userName;
+     String password;
+     String firstName;
+     String lastName;
+     LocalDate birthDate;
+     Set<String> roles;
+}
